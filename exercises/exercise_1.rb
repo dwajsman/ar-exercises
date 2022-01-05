180e22be479a0a7f2efb5374f212846c73206049
Daniel Wajsman <danielwajsman@mac.com>
2022-01-04 16:36:16 -0800

    transfer STORE code to LIB

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,23 +4,30 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
-### Exercise 1: Create 3 stores
+## Exercise 1: Create 3 stores
 
-# 1. Use Active Record's `create` class method multiple times to create 3 stores in the database:
-#   * Burnaby (annual_revenue of 300000, carries men's and women's apparel)
-#   * Richmond (annual_revenue of 1260000 carries women's apparel only)
-#   * Gastown (annual_revenue of 190000 carries men's apparel only)
-# 2. Output (`puts`) the number of the stores using ActiveRecord's `count` method, to ensure that there are three stores in the datab
-
-
-
-class Store < ActiveRecord
-    self.primary_key = "store_id"
-end
+1. Use Active Record's `create` class method multiple times to create 3 stores in the database:
+  * Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+  * Richmond (annual_revenue of 1260000 carries women's apparel only)
+  * Gastown (annual_revenue of 190000 carries men's apparel only)
+2. Output (`puts`) the number of the stores using ActiveRecord's `count` method, to ensure that there are three stores in the datab
 
 
-burnaby = Store.create(annual_revenue: 300000, apparel: ["men", "women"])
-richmond = Store.create(annual_revenue: 1260000, apparel: ["women"])
-gastown = Store.create(annual_revenue: 190000, apparel: ["men"])
+# class Store < ActiveRecord::Base
+#   # self.annual_revenue = "annual_revenue"
+#   # self.apparel = "apparel"
+# end
 
 
+# burnaby = Store.new
+# burnaby.name = "Burnaby"
+# burnaby.annual_revenue = 300000
+
+
+burnaby = Store.create(name: "Burnaby" ,annual_revenue: 300000, apparel: ["men", "women"])
+richmond = Store.create(name: "Richmond" ,annual_revenue: 1260000, apparel: ["women"])
+gastown = Store.create(name: "Gastown" ,annual_revenue: 190000, apparel: ["men"])
+
+
+puts Store.count
+# puts Store.find_by(name: 'Burnaby')
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,4 @@
 class Store < ActiveRecord::Base
-
+  self.annual_revenue = "annual_revenue"
+  self.apparel = "apparel"
 end
