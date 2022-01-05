9beb74b58777bbc26f2244852f41f9ed13a574d2
Daniel Wajsman <danielwajsman@mac.com>
2022-01-04 22:41:52 -0800

    exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.first
+@store2 = Store.second
+
+pp @store1, @store2
+
+@store1.update(name: 'Burnaby Special')
+puts "----- AFTER -----"
+pp @store1, @store2
